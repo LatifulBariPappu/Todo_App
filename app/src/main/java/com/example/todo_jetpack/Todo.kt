e746e26a3eca0838de47a96f9d889c65a432318d
Latiful <latifulbari2001@gmail.com>
2025-02-22 00:08:29 +0600

    showing fake items

--- app/src/main/java/com/example/todo_jetpack/Todo.kt
+++ app/src/main/java/com/example/todo_jetpack/Todo.kt
@@ -1,5 +1,6 @@
 package com.example.todo_jetpack
 
+import java.time.Instant
 import java.util.Date
 
 data class Todo(
@@ -7,3 +8,12 @@ data class Todo(
     var title : String,
     var createdAt : Date
 )
+
+fun getFakeTodo() : List<Todo>{
+    return listOf<Todo>(
+      Todo(1,"First todo ",Date.from(Instant.now())),
+      Todo(2,"This is my second todo ",Date.from(Instant.now())),
+      Todo(3,"The third one is here ",Date.from(Instant.now())),
+      Todo(4,"Fourth todo item ",Date.from(Instant.now()))
+    );
+}
--- app/src/main/java/com/example/todo_jetpack/TodoListPage.kt
+++ app/src/main/java/com/example/todo_jetpack/TodoListPage.kt
@@ -1,8 +1,30 @@
 package com.example.todo_jetpack
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TodoListPage(){
+    val todoList = getFakeTodo()
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ){
+        LazyColumn (
+            content = {
+                itemsIndexed(todoList){index: Int, item: Todo ->  
+                    Text(text = item.toString())
+                }
+            }
+        )
 
+    }
 }
