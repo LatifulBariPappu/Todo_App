f83f68cd78237ec4275a4af632904cce3b2c205d
Latiful <latifulbari2001@gmail.com>
2025-02-25 23:33:20 +0600

    delete icon

--- app/src/main/java/com/example/todo_jetpack/TodoListPage.kt
+++ app/src/main/java/com/example/todo_jetpack/TodoListPage.kt
@@ -8,12 +8,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.text.SimpleDateFormat
@@ -46,15 +50,24 @@ fun TodoItem(item: Todo){
             .padding(8.dp)
             .clip(RoundedCornerShape(16.dp))
             .background(MaterialTheme.colorScheme.primary)
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ){
-        Column {
+        Column (
+            modifier = Modifier.weight(1f)
+        ){
             Text(text = SimpleDateFormat("HH:mm:aa, dd/mm", Locale.ENGLISH).format(item.createdAt),
                 fontSize = 10.sp,
-                color = Color.LightGray)
+                color = Color.LightGray
+            )
             Text(text = item.title,
                 fontSize = 20.sp,
-                color = Color.White)
+                color = Color.White
+            )
+        }
+        IconButton(onClick = { }) {
+            Icon(painterResource(id = R.drawable.baseline_delete_forever_24), contentDescription = "Delete",
+                tint = Color.White)
         }
 
     }
